fd52f8c58a695c534b6d356d5ed341d0ce115381
Ben Siraphob <bensiraphob@gmail.com>
2022-03-25 13:40:27 -0500

    Overhaul highlight queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,12 +1,60 @@
-[ "new" "is" "model" "partial" "domain" "any" "fun" "of" ] @keyword
+[ "domain"
+  "model"
+  "transform"
+  "system"
+  "machine"
+  "partial"
+  "ensures"
+  "requires"
+  "conforms"
+  "includes"
+  "extends"
+  "of"
+  "returns"
+  "at"
+  "some"
+  "atleast"
+  "atmost"
+  "initially"
+  "next"
+  "property"
+  "boot"
+  "no"
+  "is"
+  "new"
+  "inj"
+  "bij"
+  "sur"
+  "fun"
+  "any"
+ ] @keyword
+
 (comment) @comment
-(real) @number
-(frac) @number
-(digits) @number
 
-(id) @variable.builtin
-(bareid) @variable.builtin
+[ (digits)
+  (real)
+  (frac)
+  ] @number
 
-
+(type_decl type: _ @type)
 (typeid) @type
 (func_term name: (_) @function.builtin)
+
+[ "+" "-" "*" "/" ":-" "::=" "::" ] @operator
+
+[ "." ] @punctuation.delimiter
+
+[ "(" ")" "{" "}" "[" "]" ] @punctuation.bracket
+
+(id) @variable.builtin
+
+(enum_cnst) @constant.builtin
+
+(field name: _ @attribute)
+(val_or_model_program name: _ @attribute)
+
+(domain_sig name: _ @module)
+(mod_ref_rename name: _ @attribute (bareid) @module)
+(mod_ref_no_rename (bareid) @module)
+
+(transform name: (bareid) @function)
